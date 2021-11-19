da714c819765295aeaf00983e660529681928c9c
zackzellto <zacharyzellto@gmail.com>
2021-11-19 11:20:44 -0700

    apple movement functionality added

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let canvas;
 let canvasContext;
+let appleImage = document.getElementById('apple-image');
 window.onload = function() {
     canvas = document.getElementById("snake-canvas");
     canvasContext = canvas.getContext("2d");
@@ -7,11 +8,12 @@ window.onload = function() {
 };
 
 function drawAll(){
+    let moveAppleWidth = Math.floor(Math.random() * (canvas.width - 15));
+    let moveAppleHeight = Math.floor(Math.random() * (canvas.height - 15));
     canvasContext.fillStyle = '#000000'
     canvasContext.fillRect(canvasContext.fillRect(0, 0, canvas.width, canvas.height),
     canvasContext.fillStyle = '#f23',
-    canvasContext.fillRect(875, 225, 20, 20),
+    canvasContext.fillRect(moveAppleWidth, moveAppleHeight, 20, 20),
     canvasContext.fillStyle = 'lightgreen',
     canvasContext.fillRect(475, 125, 160, 30),
     )}
-
